e8b40efc5beaa89f704d80bc19b3a6a6deea8cbf
feastogether <nioonn@gmail.com>
2026-05-24 18:52:05 +0800

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 window.AFIDE_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: "",
+  SUPABASE_URL: "https://chnvwziuqcqnllcjqobj.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNobnZ3eml1cWNxbmxsY2pxb2JqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2MTYxMzAsImV4cCI6MjA5NTE5MjEzMH0.wj7clsE7eO5dIE7nP4aCsDdVg1OEC3yFy7UjJpkMjWI",
   ADMIN_PIN: "123456",
   HIGHWAY_EVENTS_URL: ""
 };
